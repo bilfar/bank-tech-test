fa0816bef3e528d39019215c25eebffad9ff6d90
nabil far <nabil.far.contact@gmail.com>
2018-08-13 18:09:59 +0100

    date been created when adding a deposit

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,11 +1,23 @@
+require 'date'
 class Bank
   attr_accessor :balance
 
   def initialize(balance = 0)
     @balance = balance
+    @history = []
   end
 
-  def deposit(num)
+  def deposit(num, date = Time.now.strftime("%d/%m/%Y"))
     @balance += num
+    transaction = { date: date, credit: num, debit: "", balance: @balance }
+    @history << transaction
+  end
+
+  def print_statement
+    @history.each do |transaction|
+      return "date || credit || debit || balance
+       #{transaction[:date]} || #{transaction[:credit]} ||
+       || #{transaction[:balance]}"
+    end
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,14 +3,26 @@ require 'bank'
 # user story 1
 # as a customer
 # so i can store money in my bank-account
-# i want to deposit money in my bank-account and see my balance.
+# i want to deposit money in my bank-account and added to the balance
 
 describe Bank do
+
   subject(:bank) { Bank.new }
+
   describe '#deposit' do
     it 'should add money to the balance' do
-      bank.deposit(1000)
+      date = Time.new(2018, 8, 13).strftime("%d/%m/%Y")
+      bank.deposit(1000, date)
       expect(bank.balance).to eq(1000)
     end
+
+    it 'stores the date when action is proceed' do
+      date = Time.new(2018, 8, 13).strftime("%d/%m/%Y")
+      bank.deposit(1000, date)
+      expect(bank.print_statement).to eq("date || credit || debit || balance
+       13/08/2018 || 1000 ||
+       || 1000")
+    end
+
   end
 end
